e8ce3ddceb68051d93ff7a1dfcc1af3ce5cda291
MattM1234 <81047816+MattM1234@users.noreply.github.com>
2024-03-12 19:42:20 -0400

    Java Files

--- Project/src/main/java/com/testcode/testAPI.java
+++ Project/src/main/java/com/testcode/testAPI.java
@@ -2,6 +2,8 @@ package com.testcode;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import org.json.*;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -15,10 +17,22 @@ public class testAPI extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
+      ProbabilityCalculator Calculator = new ProbabilityCalculator();
+      Boolean hit = Boolean.parseBoolean(request.getParameter("hit"));
+      Boolean sunk = Boolean.parseBoolean(request.getParameter("sunk"));
+
+      double testCalculation[][] = Calculator.calculate(hit, 0, 0, shipSunk.none);
+      JSONArray mJSONArray = new JSONArray(Arrays.asList(testCalculation));
+
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
-      out.println("./api works. Try ./api/getMessage");
+      out.println(mJSONArray.toString());
+      //out.println("./api works. Try ./api/getMessage");      
    }
 
+   public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+   } 
 }
