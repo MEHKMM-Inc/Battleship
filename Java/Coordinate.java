f30fa003b4325fb22782e2697abb2370fa2b3fe8
Marcus <marcvs99@gmail.com>
2024-03-25 15:03:27 -0400

    updates to algorithm

---
+++ Java/Coordinate.java
@@ -0,0 +1,25 @@
+package Java;
+
+public class Coordinate 
+{
+    public int x = -1;
+    public int y = -1;
+   
+    public Coordinate()
+    {
+        x = -1;
+        y = -1;
+    }
+
+    public Coordinate(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void set(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
